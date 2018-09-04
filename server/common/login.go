6e9aad080e92b0147c3eaa75d648a766a6132c82
Reto Lehmann <ReToCode@users.noreply.github.com>
2018-09-04 10:41:56 +0200

    Feature/fixsecrets (#31)
    * - Implemented creation of service-account
    
    * Fixed incompatibility with new gin jwt lib.
    
    * Fixed incompatibility with new gin jwt lib.
    
    * Removed unnecessary fields.

--- server/common/login.go
+++ server/common/login.go
@@ -8,11 +8,18 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/jtblin/go-ldap-client"
-	"github.com/patrickmn/go-cache"
 	"gopkg.in/appleboy/gin-jwt.v2"
 )
 
-var userCache *cache.Cache
+type login struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
+type User struct {
+	UserId string
+	Email  string
+}
 
 // GetAuthMiddleware returns a gin middleware for JWT with cookie based auth
 func GetAuthMiddleware() *jwt.GinJWTMiddleware {
@@ -22,16 +29,13 @@ func GetAuthMiddleware() *jwt.GinJWTMiddleware {
 		log.Fatal("Env variable 'SESSION_KEY' must be specified")
 	}
 
-	// Initialize the user cache
-	userCache = cache.New(10*time.Minute, 24*time.Hour)
-
 	return &jwt.GinJWTMiddleware{
 		Realm:         "CLOUD_SSP",
 		Key:           []byte(key),
 		Timeout:       time.Hour,
 		MaxRefresh:    time.Hour,
 		Authenticator: ldapAuthenticator,
-		Authorizator: func(userId interface{}, c *gin.Context) bool {
+		Authorizator: func(data interface{}, c *gin.Context) bool {
 			return true
 		},
 		Unauthorized: func(c *gin.Context, code int, message string) {
@@ -46,18 +50,18 @@ func GetAuthMiddleware() *jwt.GinJWTMiddleware {
 	}
 }
 
-func userPayloadFunc(userID interface{}) jwt.MapClaims {
-	if mail, ok := userCache.Get(userID.(string)); ok {
-		res := make(map[string]interface{})
-		res["mail"] = mail
-		return res
-	} else {
-		log.Println("Error, could not find mail for user: " + userID.(string) + " - Sematext functions will not work correctly")
-		return nil
+func userPayloadFunc(data interface{}) jwt.MapClaims {
+	if v, ok := data.(*User); ok {
+		return jwt.MapClaims{
+			"id":   v.UserId,
+			"mail": v.Email,
+		}
 	}
+
+	return jwt.MapClaims{}
 }
 
-func ldapAuthenticator(userID string, password string, c *gin.Context) (interface{}, bool) {
+func ldapAuthenticator(c *gin.Context) (interface{}, error) {
 	ldapHost := os.Getenv("LDAP_URL")
 	ldapBind := os.Getenv("LDAP_BIND_DN")
 	ldapBindPw := os.Getenv("LDAP_BIND_CRED")
@@ -79,16 +83,25 @@ func ldapAuthenticator(userID string, password string, c *gin.Context) (interfac
 	// It is the responsibility of the caller to close the connection
 	defer client.Close()
 
+	var loginVals login
+	if err := c.ShouldBind(&loginVals); err != nil {
+		return "", jwt.ErrMissingLoginValues
+	}
+	userID := loginVals.Username
+	password := loginVals.Password
+
 	ok, user, err := client.Authenticate(userID, password)
 	if err != nil {
 		log.Printf("Error authenticating user %s: %+v", userID, err)
+		return nil, jwt.ErrFailedAuthentication
 	}
 	if !ok {
 		log.Printf("Authenticating failed for user %s", userID)
+		return nil, jwt.ErrFailedAuthentication
 	}
 
-	// Put the user mail address in the cache for the payload function
-	userCache.Add(userID, user["mail"], cache.DefaultExpiration)
-
-	return userID, ok
+	return &User{
+		UserId: userID,
+		Email:  user["mail"],
+	}, nil
 }
