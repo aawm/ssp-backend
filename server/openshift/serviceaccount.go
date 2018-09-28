a797f308bf8637df4bfcdbe8401d9825d7262215
Reto <reto.zeller@sbb.ch>
2018-09-28 12:32:55 +0200

    ServiceAccount Creation now works on Openshift correctly (#40)
    * add develop Branch and revert previous changes in Readme
    
    * ESTA-2733: sleep inserted in serviceaccount.go that ensures that secrets are completely created (#36)
    
    * Set Timeout in ServiceAccountCreator to 800ms (#38)
    
    * ESTA-2733: sleep inserted in serviceaccount.go that ensures that secrets are completely created
    
    * ESTA-2733: timeout in serviceaccountCreate set to 800ms, Openshift is to fast
    
    * move timeout before request the serviceaccounts (#39)
    
    * ESTA-2733: sleep inserted in serviceaccount.go that ensures that secrets are completely created
    
    * ESTA-2733: timeout in serviceaccountCreate set to 800ms, Openshift is to fast
    
    * ESTA-2733: move timeout before request the serviceaccounts

--- server/openshift/serviceaccount.go
+++ server/openshift/serviceaccount.go
@@ -110,6 +110,9 @@ func createNewServiceAccount(username string, project string, serviceaccount str
 }
 
 func createJenkinsCredential(project string, serviceaccount string, organizationKey string) error {
+	//Sleep which ensures that the serviceaccount is created completely before we take the Secret out of it.
+	time.Sleep(400 * time.Millisecond)
+
 	// Get the created service-account
 	client, saRequest := getOseHTTPClient("GET", "api/v1/namespaces/"+project+"/serviceaccounts/"+serviceaccount, nil)
 	saResponse, err := client.Do(saRequest)
@@ -132,8 +135,6 @@ func createJenkinsCredential(project string, serviceaccount string, organization
 		secret = saJson.S("secrets").Index(1)
 		secretName = strings.Trim(secret.Path("name").String(), "\"")
 	}
-	//Sleep which ensures that the token is created completely before we take the Secret.
-	time.Sleep(400 * time.Millisecond)
 
 	// Get the secret & token for the service-account
 	client, secretRequest := getOseHTTPClient("GET", "api/v1/namespaces/"+project+"/secrets/"+secretName, nil)
